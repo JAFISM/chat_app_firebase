af0fe227e64f82dc98e697575400d18b26aea7cb
Jafis <jafism2000@gmail.com>
2023-03-15 17:59:40 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:chat_app_firebase/helper/helper_function.dart';
+import 'package:chat_app_firebase/pages/auth/login_page.dart';
+import 'package:chat_app_firebase/pages/home_page.dart';
+import 'package:chat_app_firebase/shared/constants.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  if(kIsWeb){
+    // run the initialization for the web
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: Constants.apiKey,
+          appId: Constants.appId,
+          messagingSenderId: Constants.messagingSenderId,
+          projectId: Constants.projectId
+      ));
+  }
+  else{
+    // run the initialization for the android , ios
+    await Firebase.initializeApp();
+  }
+  runApp( MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  bool _isSgnedIn = false;
+
+  @override
+  void initState() {
+    getUserLoggedInStatus();
+    super.initState();
+  }
+   getUserLoggedInStatus() async{
+    await HelperFunctions.getUserLoggedInStatus().then((value) {
+      if(value!=null){
+        _isSgnedIn =value;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:_isSgnedIn?  const HomePage() : const LoginPage(),
+    );
+  }
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants{
+  static String apiKey="AIzaSyDpCnKVFcFecof3SJEMgYiUid4xFlMuah8";
+  static String appId="1:240732290824:web:ec865244459af595b936bd";
+  static String messagingSenderId="240732290824";
+  static String projectId="chatappflutter-42ed0";
+}
